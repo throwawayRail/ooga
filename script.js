d29505b3ac04d102c5eb595b69263cf2a974395b
craniacshencil <rajnikamrajnikam@gmail.com>
2024-08-04 16:13:47 +0530

    Add fully functioning temperature converter

---
+++ script.js
@@ -0,0 +1,37 @@
+const submitButton = document.querySelector(".submit-button")
+submitButton.addEventListener("click", getFormValues)
+function getFormValues() {
+    const tempValueElement = document.querySelector(".temp-input")
+    const tempUnitElement = document.querySelector(".unit-input")
+
+    const resultDiv = document.querySelector(".result")
+    const firstConversion = document.querySelector(".first-conversion")
+    const secondConversion = document.querySelector(".second-conversion")
+    firstConversion.textContent=""
+    secondConversion.textContent=""
+
+    const tempValue = Number(tempValueElement.value)
+    const tempUnit = tempUnitElement.value
+
+    var firstTemp = 0
+    var secondTemp = 0
+    if (tempUnit == "kelvin"){
+        firstTemp = tempValue - 273.15
+        secondTemp = 9 / 5 * firstTemp + 32
+        firstConversion.textContent = `${tempValue.toFixed(2)}° Kelvin = ${firstTemp.toFixed(2)}° Celsius`
+        secondConversion.textContent = `${tempValue.toFixed(2)}° Kelvin = ${secondTemp.toFixed(2)}° Fahrenheit`
+    } else if (tempUnit == "celsius") {
+        firstTemp = tempValue + 273.15
+        secondTemp = 9 / 5 * tempValue + 32
+        firstConversion.textContent = `${tempValue.toFixed(2)}° Celsius = ${firstTemp.toFixed(2)}° Kelvin `
+        secondConversion.textContent = `${tempValue.toFixed(2)}° Celsius = ${secondTemp.toFixed(2)}° Fahrenheit`
+    } else {
+        firstTemp =  5 / 9 * (tempValue - 32)
+        secondTemp = firstTemp + 273.15
+        firstConversion.textContent = `${tempValue.toFixed(2)}° Fahrenheit = ${firstTemp.toFixed(2)}° Celsius`
+        secondConversion.textContent = `${tempValue.toFixed(2)}° Fahrenheit = ${secondTemp.toFixed(2)}° Kelvin`
+    }
+
+    resultDiv.appendChild(firstConversion)
+    resultDiv.appendChild(secondConversion)
+}
